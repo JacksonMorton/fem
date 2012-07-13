049595b89cdffd53a9d5843f6780960eeb4a5edd
mlp6 <mlp6@f2fa97c2-6b19-0410-a95d-9979e3cdc715>
2012-07-13 16:31:04 +0000

    NEW repository layout using trunk and tags conventions
    git-svn-id: https://hogwarts.egr.duke.edu/svn/fem@95 f2fa97c2-6b19-0410-a95d-9979e3cdc715

---
+++ trunk/field/vf105gfp.m
@@ -0,0 +1,30 @@
+function [Th,fractionalBandwidth,centerFrequency]=vf105gfp(FIELD_PARAMS);
+% function [Th,fractionalBandwidth,centerFrequency]=vf105gfp(FIELD_PARAMS);
+%
+% Create 'Th' transducer handle and define the fractional bandwidth for the
+% VF10-5 for use by fieldprms3d_arfi.m
+% 
+% Mark 06/11/07
+
+disp('Transducer: VF10-5 (GFP setup)');
+no_elements_y=1;
+width=.2e-3;
+kerf=0.02e-3;
+pitch = width + kerf;
+height=5e-3;
+% define # of elements based on F/#
+no_elements=(FIELD_PARAMS.focus(3)/FIELD_PARAMS.Fnum)/pitch;
+no_elements = floor(no_elements);
+% lens focus
+Rfocus=20e-3;
+% mathematically sub-dice elements to make them ~1 lambda dimensions
+no_sub_y=height/width;
+no_sub_x=1;
+% define the transducer handle
+Th = xdc_focused_multirow (no_elements,width,no_elements_y,height, ...
+    kerf,kerf,Rfocus,no_sub_x,no_sub_y,FIELD_PARAMS.focus);
+% define the fractional bandwidth
+% Gianmarco's is very broadband, so I will artifcially put
+% it at 2.0
+fractionalBandwidth = 2.0;
+centerFrequency = 7.56e6;
