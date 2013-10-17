098fb4eec82537a9c7d83a51c0e83535660b5e4a
Mark Palmeri <mlp6@duke.edu>
2013-10-17 01:02:33 -0400

    work-in-progress of trying to convert matlab script ot python... mainly as a learning exercise

---
+++ field/extractAxisIntensity.py
@@ -0,0 +1,43 @@
+#!/bin/env python
+""" Extract the inteisities along a specified axis """
+
+__author__ = "Mark Palmeri"
+__email__ = "mlp6@duke.edu"
+__date__ = "2013-10-17"
+
+def extractAxisIntensity(dynaInput, axis, location, tol):
+    """
+    INPUTS: 
+        dynaInput (string) - dyna.mat file with intensity data
+        axis (int) - 1-3, based on dyna coordinates [1 = elevation, 2 = lateral, 3 = axial]
+        location (floats) - 2 element vector with locations of the orthogonal positions (dyna units, cm)
+        tol (float) - optional node search tolerance (dyna units, cm)
+
+    OUTPUTS:
+        axis (float vector) - axis positions (cm)
+        intensity (float vector) - intensities along axis
+
+    EXAMPLE: [axial,intensity]=extractAxisIntensity('dyna.mat',3,[0 0],1e-3)
+    """
+ 
+    import scipy.io
+    fieldData = scipy.io.load(dynaInput)
+    intensity = fieldData['intensity']
+    fieldParams = fieldData['FIELD_PARAMS']
+    mpn = fieldParams[0][0]
+    
+    SearchDims = list(set([1, 2, 3]) - set(axis))
+
+    import numpy as np
+    axisOfInterest = np.nonzero(
+    
+
+'''
+axisOfInterest=find(abs(mpn(:,SearchDims(1))) < (location(1) + Tol) & ...
+    abs(mpn(:,SearchDims(1))) > (location(1) - Tol) & ...
+    abs(mpn(:,SearchDims(2))) < (location(2) + Tol) & ...
+    abs(mpn(:,SearchDims(2))) > (location(2) - Tol));
+
+axis = abs(mpn(axisOfInterest,axis));
+inten = InputIntensity(axisOfInterest);
+'''
