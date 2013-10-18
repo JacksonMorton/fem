b1b506d22d53c328033c0c1b7438b5e6361dddfc
Mark Palmeri <mlp6@duke.edu>
2013-10-18 14:09:17 -0400

    direct merge of Ningri's changes; need to massage a bit

--- field/dynaField.m
+++ field/dynaField.m
@@ -1,89 +1,22 @@
-function [intensity,FIELD_PARAMS]=dynaField(FIELD_PARAMS)
-% function [intensity,FIELD_PARAMS]=dynaField(FIELD_PARAMS)
-% --------------------------------------------------------------------------
-% Generate intensity values at the nodal locations for
-% conversion to force and input into the dyna deck (performed by make_asc.m)
-% INPUTS:
-% FIELD_PARAMS.alpha (float) - absoprtion (dB/cm/MHz)
-% FIELD_PARAMS.measurementPoints - nodal locations from field2dyna.m
-% FIELD_PARAMS.Fnum (float) - F/#
-% FIELD_PARAMS.focus - [x y z] (m)
-% FIELD_PARAMS.Frequency (float) - push frequency (MHz)
-% 6.67 (VF10-5)
-% 4.21 (VF7-3)
-% FIELD_PARAMS.Transducer (string) - 'vf105'; select the
-% transducer-dependent parameters to use for the simulation
-% FIELD_PARAMS.Impulse (string) - 'guassian','exp'; use a Guassian function
-% based on the defined fractional bandwidth and center
-% frequency, or use the experimentally-measured impulse
-% response
-%
-% OUTPUT:
-% intensity - intensity values at all of the node locations
-% FIELD_PARAMS - field parameter structure
-%
-% Mark 07/31/03
-% --------------------------------------------------------------------------
-% Added frequency as an input parameter.
-% Added ability to load in experimental impulse response data.
-% Mark 01/31/05
-% --------------------------------------------------------------------------
-% Cleaned up the order of the definiting of parameters in the
-% code to make it more readable. The code now allows for the
-% transducer definition and the impulse response to be passed
-% as input variables.
-% Mark 06/16/05
-% --------------------------------------------------------------------------
-% Incorportated FIELD_PARAMS structure.
-% Mark 07/28/05
-% --------------------------------------------------------------------------
-% Fixed Transducer reference (FIELD_PARAMS.Transducer)
-% Mark 08/08/05
-% --------------------------------------------------------------------------
-% Make sure that no_elements is an integer (floor).
-% Mark 11/05/06
-% --------------------------------------------------------------------------
-% Separated the center & excitation frequencies.
-% Broke out probe definitions into separate functions.
-% Mark 06/15/07
-% --------------------------------------------------------------------------
-% Removed the estimated time; not accurate at all.
-% Mark 2010-04-20
-% --------------------------------------------------------------------------
-% Updated % processing displayed update to be more sparse and a percentage
-% to reduce file IO when running.
-% Mark 2013-02-09
-% --------------------------------------------------------------------------
+function [intensity,FIELD_PARAMS] = dynaField(FIELD_PARAMS, numWorkers)
+% allocate number of workers
+currentWorkers = matlabpool('size');
+isPoolOpen = (currentWorkers > 0);
+if (isPoolOpen)
+    matlabpool close;
+end
 
-% numWorkers = matlabpool('size');
-% isPoolOpen = (numWorkers > 0);
-% if(~isPoolOpen)
-%     matlabpool open;
-% end
+maximumNumWorkers = feature('numCores'));
 
-field_init(-1)
-%field_debug(1);
-
-disp('Starting the Field II simulation');
-%set_field('use_triangles',1);
-%set_field('use_lines',1);
-
-% define transducer-independent parameters
-set_field('c',FIELD_PARAMS.soundSpeed);
-set_field('fs',FIELD_PARAMS.samplingFrequency);
-
-% define transducer-dependent parameters
-eval(sprintf('[Th,impulseResponse]=%s(FIELD_PARAMS);',FIELD_PARAMS.Transducer));
-
-% define the impulse response
-xdc_impulse(Th,impulseResponse);
-
-% define the excitation pulse
-exciteFreq=FIELD_PARAMS.Frequency*1e6; % Hz
-ncyc=50;
-texcite=0:1/FIELD_PARAMS.samplingFrequency:ncyc/exciteFreq;
-excitationPulse=sin(2*pi*exciteFreq*texcite);
-xdc_excitation(Th,excitationPulse);
+if (nargin == 2)
+    if (numWorkers <= maximumNumWorkers)
+        matlabpool('open', numWorkers);
+    else
+        error('Invalid number of workers. Maximum is %i.', maximumNumWorkers)
+    end
+else
+    matlabpool('open', maximumNumWorkers);
+end
 
 % set attenuation
 Freq_att=FIELD_PARAMS.alpha*100/1e6; % FIELD_PARAMS in dB/cm/MHz
@@ -102,36 +35,19 @@ Time = datestr(StartTime, 'HH:MM:SS PM');
 disp(sprintf('Start Time: %s', Time))
 tic;
 
-% EstCount = 1000; % number of calculations to average over to make calc time estimates
-
-numNodes = size(FIELD_PARAMS.measurementPoints,1);
-% progressPoints = 0:10000:numNodes;
-%
-% for i=1:1000,
-%   if ~isempty(intersect(i,progressPoints)),
-%       disp(sprintf('Processed %.1f%%',i*100/numNodes));
-%   end;
-%   if i==1
-%       tic;
-%   end;
-%   [pressure, startTime] = calc_hp(Th, FIELD_PARAMS.measurementPoints(i,:));
-%   intensity(i)=sum(pressure.*pressure);
-% end
-
-%tic;
-test = Th; %not sure why, but putting Th directly inside parfor loop gives an error that Th is undefined, however, setting a different variable equal to Th and using that works.
-
-for i=1:numNodes
-%parfor i=1:100
-    [pressure, startTime] = calc_hp(test, FIELD_PARAMS.measurementPoints(i,:));
-    intensity(i)=sum(pressure.*pressure);
+spmd
+    % separate the matrices such that each core gets a roughly equal number
+    % of measurement points to perform calculations on.
+    % also, distributes matrices based on columns, rather than rows.
+    codistPoints = codistributed(FIELD_PARAMS.measurementPoints, codistributor('1d', 1));
+    pointsSize = size(FIELD_PARAMS.measurementPoints);
+    
+    
+    FIELD_PARAMS.measurementPoints = getLocalPart(codistPoints);
+    [intensityCodist,FIELD_PARAMS]=dynaField(FIELD_PARAMS);
+    
+    % combine all the separate matrices again.
+    intensityDist = codistributed.build(intensityCodist, codistributor1d(2, codistributor1d.unsetPartition, [1 pointsSize(1)]));
 end
-%intensity
-
-% matlabpool close;
-
-% CalcTime = toc; % s
-% ActualRunTime = CalcTime/60; % min
-% disp(sprintf('Actual Run Time = %.3f m\n',ActualRunTime));
-
-field_end;
+intensity = gather(intensityDist);
+matlabpool close
